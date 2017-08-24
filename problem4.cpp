13889a1e2b1625e113b3d8a8d3f73150b6aa00f3
ManishaSahu6 <31317939+ManishaSahu6@users.noreply.github.com>
2017-08-24 21:58:32 +0530

    Create problem4.cpp

---
+++ problem4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int l,b,p;
+	cout << "Enter length: " << endl;
+	cin >> l;
+	cout << "Enter breadth:" << endl;
+	cin >> b;
+	p=2*(l+b);
+	cout << "Perimeter=" << p;
+	
+}
