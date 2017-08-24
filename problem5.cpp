1a3b6c950cc129d4695ab56179b54d7c9bf352fb
ManishaSahu6 <31317939+ManishaSahu6@users.noreply.github.com>
2017-08-24 22:00:08 +0530

    Create problem5.cpp

---
+++ problem5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int l,b,a;
+	cout << "Enter length: " << endl;
+	cin >> l;
+	cout << "Enter breadth:" << endl;
+	cin >> b;
+	a=l*b;
+	cout << "Area=" << a;
+	
+}
