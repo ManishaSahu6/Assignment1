ca68934e356128cd5eb33ebfb4dcae7762749eef
ManishaSahu6 <31317939+ManishaSahu6@users.noreply.github.com>
2017-08-24 22:04:04 +0530

    Create problem6.cpp

---
+++ problem6.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int r,d;
+	float c,a;
+	cout << "Enter radius: " << endl;
+	cin >> r;
+	d=2*r;
+	cout << "Diameter=" << d << endl;
+	c=2*3.14*r;
+	cout << "Circumference=" << c << endl;
+	
+	a=3.14*r*r;;
+	cout << "Area=" << a;
+	
+}
